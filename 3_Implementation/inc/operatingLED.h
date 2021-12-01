2d3c2ff0e2844b599c34ef03d27211189b7d338a
santhosh1120 <94516294+santhosh1120@users.noreply.github.com>
2021-12-01 13:00:22 +0530

    Update and rename user_utils.h to operatingLED.h

---
+++ 3_Implementation/inc/operatingLED.h
@@ -0,0 +1,11 @@
+#ifndef _operatingLED_H_
+#define _operatingLED_H_
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void operatingLED_on();
+void operatingLED_off();
+
+#endif
